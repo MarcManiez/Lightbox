7bd3796460e3a5dce2d3a2c1c7702d6d358604a0
MarcManiez <marc.maniez@gmail.com[A>
2017-02-05 23:23:01 -0800

    Fit methods into an app object.

--- index.js
+++ index.js
@@ -1,36 +1,44 @@
-const httpRequest = new XMLHttpRequest();
-const url = `https://api.flickr.com/services/rest/?
-  method=flickr.photosets.getPhotos&
-  api_key=426a1c2adbd9c8569c74d4a8973068ba&
-  format=json&
-  photoset_id=72157626579923453`;
-let photoSet;
+const app = {
 
-const createPictureNode = function({ farm, id, server, secret}, size) {
-  const src = `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}_${size || 'n'}.jpg`;
-  const tile = document.createElement('div');
-  tile.setAttribute('class', 'tile');
-  const imageContainer = document.createElement('div');
-  imageContainer.setAttribute('class', 'image-container');
-  const image = document.createElement('img');
-  image.setAttribute('src', 'https://farm4.staticflickr.com/3492/5713357208_a164f169c6_n.jpg');
-  imageContainer.appendChild(image);
-  tile.appendChild(imageContainer);
-  document.getElementsByClassName('tiles')[0].appendChild(tile);
-  return tile;
+  photoset: null,
+
+  init: function() {
+    const url = `https://api.flickr.com/services/rest/?
+      method=flickr.photosets.getPhotos&
+      api_key=426a1c2adbd9c8569c74d4a8973068ba&
+      format=json&
+      photoset_id=72157626579923453`;
+    const httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function() {
+      if (httpRequest.readyState === XMLHttpRequest.DONE) {
+        if (httpRequest.status === 200) {
+          const jsonFlickrApi = (object) => object;   // matches the name of wrapper function inside of eval(httpRequest.responseText) 
+          app.photoSet = eval(httpRequest.responseText).photoset.photo;
+          for (let i = 0; i < app.photoSet.length; i++) {
+            app.createImageNode(app.photoSet[i]);
+          }
+        } else {
+          console.err('request failed');
+        }
+      }
+    };
+    httpRequest.open('GET', url, true);
+    httpRequest.send();
+  },
+
+  createImageNode: function({ farm, id, server, secret}, size) {
+    const src = `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}_${size || 'n'}.jpg`;
+    const tile = document.createElement('div');
+    tile.setAttribute('class', 'tile');
+    const imageContainer = document.createElement('div');
+    imageContainer.setAttribute('class', 'image-container');
+    const image = document.createElement('img');
+    image.setAttribute('src', src);
+    imageContainer.appendChild(image);
+    tile.appendChild(imageContainer);
+    document.getElementsByClassName('tiles')[0].appendChild(tile);
+    return tile;
+  },
 };
 
-httpRequest.onreadystatechange = function(data) {
-  if (httpRequest.readyState === XMLHttpRequest.DONE) {
-    if (httpRequest.status === 200) {
-      const jsonFlickrApi = (object) => object;
-      photoSet = eval(httpRequest.responseText);
-      console.log(photoSet);
-    } else {
-      console.err('request failed');
-    }
-  }
-};
-
-httpRequest.open('GET', url, true);
-httpRequest.send();
+app.init();
