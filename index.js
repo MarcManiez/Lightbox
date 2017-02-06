e1309f649fde05a739994c258445919818326a66
MarcManiez <marc.maniez@gmail.com[A>
2017-02-06 11:03:06 -0800

    Decouple GET request and photo rendering.

--- index.js
+++ index.js
@@ -1,30 +1,38 @@
 const app = {
 
   photoset: null,
+  flickrApiUrl: `https://api.flickr.com/services/rest/?
+    method=flickr.photosets.getPhotos&
+    api_key=426a1c2adbd9c8569c74d4a8973068ba&
+    format=json&
+    photoset_id=72157626579923453`,
 
   init: function() {
-    const url = `https://api.flickr.com/services/rest/?
-      method=flickr.photosets.getPhotos&
-      api_key=426a1c2adbd9c8569c74d4a8973068ba&
-      format=json&
-      photoset_id=72157626579923453`;
+    app.getImages(app.displayImages);
+  },
+
+  getImages: function(callback) {
     const httpRequest = new XMLHttpRequest();
     httpRequest.onreadystatechange = function() {
       if (httpRequest.readyState === XMLHttpRequest.DONE) {
         if (httpRequest.status === 200) {
           const jsonFlickrApi = (object) => object;   // matches the name of wrapper function inside of eval(httpRequest.responseText) 
           app.photoSet = eval(httpRequest.responseText).photoset.photo;
-          for (let i = 0; i < app.photoSet.length; i++) {
-            const src = app.createImageUrl(app.photoSet[i]);
-            app.createImageNode(src);
-          }
+          callback(app.photoSet);
         } else {
           console.err('request failed');
         }
       }
     };
-    httpRequest.open('GET', url, true);
+    httpRequest.open('GET', app.flickrApiUrl, true);
     httpRequest.send();
+  },
+
+  displayImages: function(photos) {
+    for (let i = 0; i < photos.length; i++) {
+      const src = app.createImageUrl(photos[i]);
+      app.createImageNode(src);
+    }
   },
 
   createImageNode: function(src) {
